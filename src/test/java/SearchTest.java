fe503853f066226dc0b8b87f89923229e4d53f27
rekogz <rekogz@gmail.com>
2023-07-20 18:06:55 +0500

    ashalet

---
+++ src/test/java/SearchTest.java
@@ -0,0 +1,17 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchTest {
+    @Test
+    void successfulSearchTest() {
+        Configuration.browser = "firefox";
+
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+    }
+}
